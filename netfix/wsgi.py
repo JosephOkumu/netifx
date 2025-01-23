35ac74bdeedb4888007871119578f4fccc29bc0d
JosephOkumu <jokumu25@gmail.com>
2025-01-23 21:01:39 +0300

    update wsgi.py

--- netfix/wsgi.py
+++ netfix/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "netfix.settings")
+# Set the default settings module for the 'netfix' project
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'netfix.settings')
 
+# Create the WSGI application
 application = get_wsgi_application()
+
