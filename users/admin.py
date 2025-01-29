277df4633d23d3f3a2def7dbdf08c1d586ff0a18
JosephOkumu <jokumu25@gmail.com>
2025-01-29 21:21:20 +0300

    feat: add user id and birth details

--- users/admin.py
+++ users/admin.py
@@ -6,16 +6,13 @@ from .models import User, Customer, Company
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ("id", "username", "email")
-    search_fields = ("username", "email")  # Added for search functionality
 
 
 @admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
-    list_display = ("user", "field")
-    search_fields = ("user__username", "field")  # Added search by user and field
+    list_display = ("user_id", "field")
 
 
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ("user", "birth")
-    search_fields = ("user__username",)  # Added search by username
+    list_display = ("user_id", "birth")
