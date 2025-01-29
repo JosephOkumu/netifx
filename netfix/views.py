24cb10b0974b3ad329e2e8312535e57704b64eee
JosephOkumu <jokumu25@gmail.com>
2025-01-29 21:12:28 +0300

    feat: add functions for returning customer and company profile

--- netfix/views.py
+++ netfix/views.py
@@ -1,23 +1,48 @@
-from django.shortcuts import render, get_object_or_404
-from users.models import User, Company
-from services.models import Service
+from django.shortcuts import render
 
+from users.models import User, Company,Customer
+from services.models import Service,Service_Request
+from datetime import date
 
 def home(request):
     return render(request, 'users/home.html', {'user': request.user})
+def calculate_age(birth_date):
+    """Calculates the age of a user based on their birth date"""
+    today = date.today()
+    age = today.year - birth_date.year - ((today.month, today.day) < (birth_date.month, birth_date.day))
+    return age
 
 
-def customer_profile(request):
-    # TODO: Implement the customer profile view if needed
-    pass
-
+def user_profile(request, name):
+    user = User.objects.get(username=name)
+    if user.is_customer:
+        return customer_profile(request, name)
+    elif user.is_company:
+        return company_profile(request, name)
+    else:
+        return ("Invalid user type")
+def customer_profile(request, name):
+    user = User.objects.get(username=name)
+    service_request = Service_Request.objects.filter(user_id=user.id)
+    user_age = None
+    if user.is_customer:
+        customer = Customer.objects.get(user_id=user.id)
+        birth_date = customer.birth
+       
+        user_age = calculate_age(birth_date)
+        
+   
+    return render(request, 'users/profile.html', {'user': user, 'user_age': user_age,'service_request' :service_request})
 
 def company_profile(request, name):
-    # Use get_object_or_404 to handle cases where user or company might not exist
-    user = get_object_or_404(User, username=name)
-    company = get_object_or_404(Company, user=user)
+    user = User.objects.get(username=name)
+    company_id =user.id
+    user_age = None
+    user_company = None  
+        
+    
 
-    # Fetch all the services provided by the company and order them by date
-    services = Service.objects.filter(company=company).order_by("-date")
-
-    return render(request, 'users/profile.html', {'user': user, 'services': services})
+    services = Service.objects.filter(name_company=name).order_by("-date")
+    company = Company.objects.get(user_id=company_id)
+    user_company = company.field
+    return render(request, 'users/profile.html', {'user': user, 'user_age': user_age, 'user_company': user_company,'services':services})
