3d324af7ce559c89a315e6eceba9a2c4504e0e06
JosephOkumu <jokumu25@gmail.com>
2025-01-29 21:12:59 +0300

    refactor: modify settings

--- netfix/wsgi.py
+++ netfix/wsgi.py
@@ -8,11 +8,9 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+
 from django.core.wsgi import get_wsgi_application
 
-# Set the default settings module for the 'netfix' project
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'netfix.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "netfix.settings")
 
-# Create the WSGI application
 application = get_wsgi_application()
-
