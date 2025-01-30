1c5744d4194d3a7268a5faca79d6a9f7c5335d6b
JosephOkumu <jokumu25@gmail.com>
2025-01-30 19:46:10 +0300

    refactor: rename fields and add comments

--- main/views.py
+++ main/views.py
@@ -1,35 +1,33 @@
 from django.shortcuts import render
 from django.contrib.auth import logout as django_logout
-from services.models import Service,Service_Request
+from services.models import Service, Service_Request
 from django.db.models import Count
 
-
 def home(request):
+    # Query to count how many times each service has been requested, ordered by most requested
     service_counts = (
         Service_Request.objects
         .values('service_id')
-        .annotate(count=Count('service_id'))
-        .order_by('-count')
+        .annotate(count=Count('service_id'))  # Count how many times each service has been requested
+        .order_by('-count') 
     )
 
-    service_Mosts = []
+    service_Mosts = []  
+
     for item in service_counts:
-        service_id = item['service_id']
-        # service_count = item['count']
-        service = Service.objects.get(id=service_id)
+        service_id = item['service_id'] 
+        service = Service.objects.get(id=service_id)  
         service_Mosts.append({
-            'id' : service.id,
-            'nom': service.name,
-            'field': service.field,
-            'prix': service.price_hour,
-            'company': service.name_company,
+            'id': service.id,  
+            'name': service.name, 
+            'field': service.field,  
+            'price': service.price_hour,  
+            'company': service.name_company,  
         })
 
-    context = {'service_Mosts': service_Mosts[0:3]}
-    return render(request, 'main/home.html', context)
-
-
+    context = {'service_Mosts': service_Mosts[0:3]}  # Limit the list to the top 3 most requested services
+    return render(request, 'main/home.html', context)  # Render the 'home.html' template with the context data
 
 def logout(request):
-    django_logout(request)
-    return render(request, "main/logout.html")
+    django_logout(request)  
+    return render(request, "main/logout.html") 
